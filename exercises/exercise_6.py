abe8598a8f5d1fad6b8e50404ecfd0e5776560d3
Ibraheemabdallah123 <ibraheemabdallah1313@gmail.com>
2024-06-03 10:59:22 +0000

    task 6

--- exercises/exercise_6.py
+++ exercises/exercise_6.py
@@ -1,2 +1,11 @@
 # Your solution to Exercise 6
-
+point1 = int(input("input the x of a point on a graph"))
+point11 = int(input("input the y of a point on a graph"))
+point2 = int(input("input the x of a point on a graph"))
+point22 = int(input("input the y of a point on a graph"))
+if point1 + point11 < point2 + point22:
+  print("A is closer to the origin")
+elif point1 + point11 > point2 + point22:
+  print("B is closer to the origin")
+else:
+  print("A and B are the same distance from the origin")
