33fb68d62ca1ed6772e311a04ceb2663ef0a2657
Ibraheemabdallah123 <ibraheemabdallah1313@gmail.com>
2024-06-03 11:03:47 +0000

    task 12

--- exercises/exercise_12.py
+++ exercises/exercise_12.py
@@ -1,2 +1,11 @@
 # Your solution to Exercise 12
-
+num = int(input("Enter a four digit number: "))
+thousand = num // 1000
+hundred = (num % 1000) // 100
+tens = (num % 100) // 10
+ones = num % 10
+thousand = "*" if thousand % 2 == 0 else str(thousand)
+hundred = "*" if hundred  % 2 == 0 else str(hundred)
+tens = "*" if tens  % 2 == 0 else str(tens)
+ones = "*" if ones  % 2 == 0 else str(ones)
+print(thousand, hundred, tens, ones)
