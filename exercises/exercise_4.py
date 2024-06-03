6bb9df2ce1a2c291f7ba9239d20ee3773aa9b35f
Ibraheemabdallah123 <ibraheemabdallah1313@gmail.com>
2024-06-03 10:57:07 +0000

    task 4

--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -1,2 +1,12 @@
 # Your solution to Exercise 4
-
+grade = int(input("enter your grade "))
+if grade < 4:
+  print("initial level")
+elif grade < 7:
+  print("average level")
+elif grade < 10:
+  print("sufficient level")
+elif grade < 13:
+  print("high level")
+else:
+  print("level is absent")
