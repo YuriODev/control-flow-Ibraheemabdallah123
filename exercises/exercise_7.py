b3b2c977d695cd51e696a522ad521ecfdf142938
Ibraheemabdallah123 <ibraheemabdallah1313@gmail.com>
2024-06-03 11:00:12 +0000

    task 7

--- exercises/exercise_7.py
+++ exercises/exercise_7.py
@@ -1,2 +1,20 @@
 # Your solution to Exercise 7
-
+num1 = float(input("number 1 "))
+num2 = float(input("number 2 "))
+operation = input("operation")
+if operation == "+":
+  print(num1 + num2)
+elif operation == "-":
+  print(num1 - num2)
+elif operation == "*":
+  print(num1 * num2)
+elif operation == "/" and num2 != 0:
+  print(num1 / num2)
+elif operation == "mod" and num2 != 0:
+  print(num1 % num2)
+elif operation == "pow":
+  print(num1 ** num2)
+elif operation == "div" and num2 != 0:
+  print(num1 // num2)
+else:
+  print("error")
